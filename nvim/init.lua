88fee9a98c8944ca2865acc106de959edc652cfe
praful <220836+Praful@users.noreply.github.com>
2024-06-25 00:33:24 +0100

    hide signcolumn when running firenvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -27,7 +27,9 @@ vim.api.nvim_create_autocmd({'UIEnter'}, {
     callback = function(event)
         local client = vim.api.nvim_get_chan_info(vim.v.event.chan).client
         if client ~= nil and client.name == "Firenvim" then
-            vim.o.laststatus = 0
+            vim.opt.laststatus = 0
+            vim.opt.number = false
+            vim.opt.signcolumn = "no"
         end
     end
 })
